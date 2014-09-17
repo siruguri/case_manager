7c56e0a898e6a06e54f9c2a8ff7300c937646c38
Sameer Siruguri <siruguri@gmail.com>
2014-09-17 09:42:30 -0700

    volunteers can see which clients need special attention

--- app/models/form_cross_reference.rb
+++ app/models/form_cross_reference.rb
@@ -1,3 +1,16 @@
 class FormCrossReference < ActiveRecord::Base
   belongs_to :form_entry
+
+  def cross_reference_instance
+    if self.cross_reference_class
+      klass = Module.const_get self.cross_reference_class
+      # maybe this could be klass.respond_to? :find instead
+      if klass.is_a?(Class) and klass.superclass == ActiveRecord::Base
+        return klass.send(:find, self.cross_reference_id)
+      end
+    end
+
+    return nil
+  end
+      
 end
--- app/models/form_entry.rb
+++ app/models/form_entry.rb
@@ -4,4 +4,16 @@ class FormEntry < ActiveRecord::Base
   has_many :form_cross_references
   has_many :multiple_choice_entries
 
+
+  def cross_reference(xref_type: :client)
+    if xref_type == :client
+      client = self.form_cross_references.select { |x| x.cross_reference_class.underscore.to_sym == xref_type }
+      puts ">>> #{client}"
+      if client 
+        return client[0].cross_reference_instance
+      else
+        return nil
+      end
+    end
+  end
 end
