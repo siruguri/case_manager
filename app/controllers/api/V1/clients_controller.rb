6cfcf2beeb4de48881eda16202d2d693c6b3222a
Sameer Siruguri <siruguri@gmail.com>
2014-10-04 06:05:16 -0700

    added access control to clients api

--- app/controllers/api/V1/clients_controller.rb
+++ app/controllers/api/V1/clients_controller.rb
@@ -12,9 +12,11 @@ module Api
         puts ">> Searching for #{search_attr} like #{params[:q]}"
         @clients = Client.where "#{search_attr} like ?", "%#{params[:q]}%"
         autolist = @clients.map do |ct|
-          {id: ct.id, label: ct.send(search_attr.to_sym), last_name: ct.last_name,
-            first_name: ct.first_name, mr_number: ct.mr_number}
-        end
+          if can?(:read, ct)
+            {id: ct.id, label: ct.send(search_attr.to_sym), last_name: ct.last_name,
+              first_name: ct.first_name, mr_number: ct.mr_number}
+          end
+        end.compact
         
         puts ">> response #{autolist.to_json}"
 
