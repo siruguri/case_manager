f0e66de2fb603e13b7c66692123f34b5eaa512bd
Sameer Siruguri <siruguri@gmail.com>
2014-10-13 17:25:35 -0700

    refactored form structures seeds

--- db/seeds/make_form_structures.rb
+++ db/seeds/make_form_structures.rb
@@ -1,17 +1,63 @@
 class IncorrectSeedData < Exception
 end
 
+def load_mc_qns(mc_qns)
+  mc_qns.each do |qn_h|
+    qn = MultipleChoiceQuestion.find_or_initialize_by(key: qn_h[:key])
+    qn.display_value = qn_h[:dv]
+    qn.is_boolean = (qn_h[:is_boolean] == true)
+    if qn_h[:choices]
+      qn.choices = qn_h[:choices]
+    elsif qn.is_boolean
+      qn.choices = MultipleChoiceQuestion.boolean_choice_array
+    else
+      raise IncorrectSeedData, "Seed (choices) data for qn #{qn_h} is incomplete"
+    end
+    qn.save
+  end
+end
+
+def load_freetext_qns(free_text_qns)
+  free_text_qns.each do |qn_h|
+    qn=FreeTextQuestion.find_or_initialize_by(key: qn_h[:key])
+    qn.display_value = qn_h[:dv]
+    qn.save
+  end
+end
+
+def initialize_form_structure(name, order)
+  s = FormStructure.find_by_form_name name
+  order.each_with_index do |key, idx|
+    qn = [MultipleChoiceQuestion.find_by_key(key), FreeTextQuestion.find_by_key(key)].compact.first
+    s.add_question qn, idx
+  end
+end
+
 fs=FormStructure.find_or_create_by(id: 1)
+fs.form_name='Eligibility'
+fs.author = User.find_by_email('cho_encore.mgr@casemanager.com')
+fs.save
+cho_eligibility_form=fs
+
+fs=FormStructure.find_or_create_by(id: 2)
 fs.form_name='Enrolment'
 fs.author = User.find_by_email('cho_encore.mgr@casemanager.com')
 fs.save
+cho_enrolment_form=fs
 
+fs=FormStructure.find_or_create_by(id: 3)
+fs.form_name='START Survey'
+fs.author = User.find_by_email('youth_alive.mgr@casemanager.com')
+fs.save
+ya_start_form=fs
+
+# CHO Encore Eligibility
 free_text_qns =\
 [{key: :no_study_reason, dv: 'If you are not participating in the study, please describe why.'},
  {key: :no_primary_language, dv: 'If your primary language is not listed, please specify it here.'},
  {key: :no_alameda_resident, dv: 'If you\'re not a resident of Alameda County, please enter your county of residence.'},
  {key: :general_comments, dv: 'Comments'},
-] 
+]; load_freetext_qns free_text_qns
 
 mc_qns = 
 [{key: :language_choice, dv: 'Which language would you like to answer questions in?', choices: %w(English Spanish Other)},
@@ -22,44 +68,15 @@ mc_qns =
  {key: :already_participating, dv: 'Are you already participating in this study?', is_boolean: true},
  {key: :resident_of_alameda, dv: 'Are you a resident of Alameda County?', is_boolean: true},
  {key: :foster_care_child, dv: 'Is your child a foster care child or in clinic for protective clearance exam?', is_boolean: true},
-]
+]; load_mc_qns mc_qns
 
-enrolment_order=\
+eligibility_order=\
 [ :language_choice, :study_participate, :no_study_reason, :primary_language, :no_primary_language, :primary_caregiver, 
   :over_18, :already_participating, :resident_of_alameda, :no_alameda_resident, :foster_care_child, :general_comments]
 
-mc_qns.each do |qn_h|
-  qn = MultipleChoiceQuestion.find_or_initialize_by(key: qn_h[:key])
-  qn.display_value = qn_h[:dv]
-  qn.is_boolean = (qn_h[:is_boolean] == true)
-  if qn_h[:choices]
-    qn.choices = qn_h[:choices]
-  elsif qn.is_boolean
-    qn.choices = MultipleChoiceQuestion.boolean_choice_array
-  else
-    raise IncorrectSeedData, "Seed (choices) data for qn #{qn_h} is incomplete"
-  end
-  qn.save
-end
+initialize_form_structure 'Enrolment', eligibility_order
 
-free_text_qns.each do |qn_h|
-  qn=FreeTextQuestion.find_or_initialize_by(key: qn_h[:key])
-  qn.display_value = qn_h[:dv]
-  qn.save
-end
-
-# Add all of these qns to the above form structure
-s = FormStructure.find_by_form_name 'Enrolment'
-enrolment_order.each_with_index do |key, idx|
-  qn = [MultipleChoiceQuestion.find_by_key(key), FreeTextQuestion.find_by_key(key)].compact.first
-  s.add_question qn, idx
-end
-
-fs=FormStructure.find_or_create_by(id: 2)
-fs.form_name='START Survey'
-fs.author = User.find_by_email('youth_alive.mgr@casemanager.com')
-fs.save
-
+# YA START Survey
 four_parts=%w(Never Rarely Sometimes Often)
 mc_qns = 
 [{key: :everyday_focus, dv: 'In the past 12 months have you had trouble focusing on everyday tasks, like working, doing chores, running errands or reading?', choices: four_parts},
@@ -69,20 +86,23 @@ mc_qns =
  {key: :on_guard, dv: 'In the past 12 months, how often did you feel constantly on guard, watchful or jumpy?', choices: four_parts},
  {key: :avoid_situation, dv: 'In the last 12 months, have you tried hard not to think about it or went out of your way to avoid situations that reminded you of it?', choices: four_parts},
  {key: :experience_in_30, dv: 'How often over the past 30 days did you experience any of these feelings? Would you say:', choices: four_parts}
-]
+]; load_mc_qns mc_qns
 
-mc_qns.each do |qn_h|
-  qn = MultipleChoiceQuestion.find_or_initialize_by(key: qn_h[:key])
-  qn.display_value = qn_h[:dv]
-  qn.choices = qn_h[:choices]
-  qn.save
-end
-
-enrolment_order=\
+form_order=\
 [:everyday_focus, :problems_sleeping, :disturbing_memories, :feel_numb, :on_guard, :avoid_situation, :experience_in_30]
 
-enrolment_order.each_with_index do |key, idx|
-  qn = MultipleChoiceQuestion.find_by_key(key)
-  fs.add_question qn, idx
-end
+initialize_form_structure 'START Survey', form_order
+
+mc_qns = 
+[{key: :full_intervention_day, dv: 'Full Intervention Day?', is_boolean: true},
+ {key: :enrolment_consent, dv: 'Consent', is_boolean: true},
+ {key: :intervention_plan_completed, dv: 'Intervention Plan completed?', is_boolean: true},
+ {key: :qualtrics_completed, dv: 'Qualtrics Staff Survey Completed?', is_boolean: true},
+]; load_mc_qns mc_qns
+
+free_text_qns =\
+[
+{key: :vol_name, dv: 'Volunteer Name'},
+{key: :date_of_visit, dv: 'Date of visit'},
+]; load_freetext_qns free_text_qns
 
