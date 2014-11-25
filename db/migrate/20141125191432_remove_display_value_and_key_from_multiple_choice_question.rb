e4b87e3f0d31e02ee980dd09d5be88d9e22d215b
Sameer Siruguri <siruguri@gmail.com>
2014-11-25 11:50:52 -0800

    bugs in massive update

--- db/migrate/20141125191432_remove_display_value_and_key_from_multiple_choice_question.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDisplayValueAndKeyFromMultipleChoiceQuestion < ActiveRecord::Migration
-  def change
-    remove_column :multiple_choice_questions, :display_value
-    remove_column :multiple_choice_questions, :key
-  end
-end
--- db/migrate/20141125191433_remove_display_value_and_key_from_free_text_question.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDisplayValueAndKeyFromFreeTextQuestion < ActiveRecord::Migration
-  def change
-    remove_column :free_text_questions, :display_value
-    remove_column :free_text_questions, :key
-  end
-end
